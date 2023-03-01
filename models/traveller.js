0abb60b5700e87b5deda14c46fc9a749887554c4
Chris Barclay <chris.barclay1@gmail.com>
2023-03-01 16:49:10 +0000

    Testing Completed

--- models/traveller.js
+++ models/traveller.js
@@ -3,28 +3,29 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
-};
+  return startLocations = this.journeys.map(journey => journey.startLocation)
+}; //MAP
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
-};
+  return endLocations = this.journeys.map(journey => journey.endLocation)
+}; //MAP
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
-};
+  return journeyByTransport = this.journeys.filter(journey => (journey.transport === transport))
+}; //FILTER
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
-};
+  return journeysOverDistance = this.journeys.filter(journey => (journey.distance >= minDistance))
+}; //FILTER
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
-};
+  return totalDistanceTravelled = this.journeys.reduce((runningTotal, journey) => (runningTotal += journey.distance), 0)
+}; //REDUCE
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
+  const mapTransport = this.journeys.map(journey => journey.transport);
+  return Array.from(new Set(mapTransport))
+}; //FILTER
 
 
 module.exports = Traveller;
